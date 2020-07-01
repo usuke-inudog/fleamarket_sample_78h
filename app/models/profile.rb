a75653f5a1b4c1ded533eefa55ba25b51183882d
jo-taichi <strarf2215@yahoo.co.jp>
2020-07-01 21:12:34 +0900

    全角指定のバリデーション追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,14 @@
 class Profile < ApplicationRecord
-  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, presence: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana,
+            :birth_year, :birth_month, :birth_day, presence: true
+
+  validates :family_name, :first_name,
+            format: { with: /^[一-龥ぁ-ん]/ }
+            # 全角ひらがな、漢字
+
+  validates :family_name_kana, :first_name_kana,
+            format: { with: /^[ぁ-ん]+$/ }
+            # 全角ひらがな
 
   belongs_to :user
 
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,5 +1,15 @@
 class ShippingAddress < ApplicationRecord
-  validates :shipping_family_name, :shipping_first_name, :shipping_family_name_kana, :shipping_first_name_kana, :postal_code, :state_province, :city, :street, :phone_number, presence: true
+  validates :shipping_family_name, :shipping_first_name, :shipping_family_name_kana, :shipping_first_name_kana,
+            :postal_code, :state_province, :city, :street, :phone_number, presence: true
+
+  validates :shipping_family_name, :shipping_first_name,
+            format: { with: /^[一-龥ぁ-ん]/ }
+            # 全角ひらがな、漢字
+
+  validates :shipping_family_name_kana, :shipping_first_name_kana,
+            format: { with: /^[ぁ-ん]+$/ }
+            # 全角ひらがな
+  
   validates :postal_code, length: { is: 7 }
 
   belongs_to :user
