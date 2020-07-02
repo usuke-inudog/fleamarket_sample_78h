18c52a1f6b00b3580292d670fd7a38365450c9cf
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-02 16:51:21 +0900

    add_delivery_budens_table

---
+++ app/models/delivery_burden.rb
@@ -0,0 +1,2 @@
+class DeliveryBurden < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_074759) do
+ActiveRecord::Schema.define(version: 2020_07_02_075034) do
+
+  create_table "delivery_burdens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "burden"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "delivery_methods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "method"
