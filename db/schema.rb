c7a664d5b9f8bcb5c153262927909f289dc70d12
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-02 16:47:12 +0900

    add_shipping_day&shipper_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_074207) do
+ActiveRecord::Schema.define(version: 2020_07_02_074605) do
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 2020_07_02_074207) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "shippers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "shipper"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "shipping_family_name", null: false
     t.string "shipping_first_name", null: false
@@ -43,6 +49,12 @@ ActiveRecord::Schema.define(version: 2020_07_02_074207) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_shipping_addresses_on_user_id"
+  end
+
+  create_table "shipping_days", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "shipping_day"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
