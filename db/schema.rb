80486bf41872a044973a6b5c853bfc2b2af8e927
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-02 16:48:46 +0900

    add_delivery_methods_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_074605) do
+ActiveRecord::Schema.define(version: 2020_07_02_074759) do
+
+  create_table "delivery_methods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "method"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
