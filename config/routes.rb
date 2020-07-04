0e950bccc1b6fdc5366eb07704b6bbf5286c7098
8hachi8 <bold.ryst@gmail.com>
2020-07-04 21:45:51 +0900

    商品詳細マークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :show]
   devise_for :users
 end
