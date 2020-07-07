e8be92da3cfcf350fa56e8e034c61840505ffa1b
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-07 16:17:51 +0900

    fixed_controller_&_routes
    routes.rbの記述の消し忘れ、コントローラーの冗長なコードを訂正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   def index
     # 現在カテゴリーの親階層は13種類のため、limit(13)
-    @parents = Category.all.order("id ASC").limit(13)
+    @parents = Category.order("id ASC").limit(13)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'categories/index'
   root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
