15921e044dd5623b271e930717153f3064a7e6a8
8hachi8 <bold.ryst@gmail.com>
2020-07-08 17:22:49 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   belongs_to :shipper
   belongs_to :shipping_day
   belongs_to :size
-  has_many   :item_images
+  has_many   :item_images, dependent: :destroy
+  accepts_nested_attributes_for :itme_images, allow_destroy: true
   belongs_to :seller, class_name:"User", optional: true, foreign_key: "seller_id"
   belongs_to :buyer, class_name:"User", optional: true, foreign_key: "buyer_id"
 end
