631c1b6934ccd6f3c38165b9198347bfb44b02f8
8hachi8 <bold.ryst@gmail.com>
2020-07-08 17:22:57 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :new, :show]
   devise_for :users
   resources   :categories, only: [:index, :show]
   
