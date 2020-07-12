59e5f334751ce87910a9d5c532d14fada2a278f8
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-12 23:04:13 +0900

    add_sell_function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,13 +5,30 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item.build_brand
+    @item.item_images.build
   end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @item.item_images.new
+      render :new
+    end
+  end
+
   def show
   end
 
   private
   def set_category
     @parents = Category.where(ancestry: nil).order("id ASC")
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, [brand_attributes: [:id, :name]], :category_id, :item_condition, :delivery_burden, :delivery_method, :shipper, :shipping_day, :size, [item_images_attributes: [:image, :_destroy, :id]]).merge(seller_id: current_user.id)
   end
 end
