73757b3363cd2c3b57871e1ecb4a18bc8a4300a1
8hachi8 <bold.ryst@gmail.com>
2020-07-14 15:38:26 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :user do
-    password = Faker::Internet.password(min_length: 7)
+    password = Faker::Internet.password(min_length: 7, max_length: 128)
     sequence(:nickname)   {Faker::Name.name}
     sequence(:email)      {Faker::Internet.email}
     password              {password}
