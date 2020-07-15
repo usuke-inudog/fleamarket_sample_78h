aad400dcf8267b631ff27a156d5ef236c5ac64e3
usuke-inudog <66010511+usuke-inudog@users.noreply.github.com>
2020-07-15 20:30:01 +0900

    add_buy_check_page

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :categories, only: [:index, :show]
   resources :credit_cards
+  resources :purchase, only: :show
 end
