53acde5dd52b69883be99ed4ae8be64e8d70c9b6
jo-taichi <strarf2215@yahoo.co.jp>
2020-07-17 21:38:35 +0900

    商品購入機能の基幹実装

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,11 +1,71 @@
 class PurchaseController < ApplicationController
+  before_action :set_item, only: [:show, :pay]
+  before_action :correct_user, only: [:show]
+  before_action :secret_key, only: [:show, :pay]
+  before_action :set_card, only: [:show, :pay]
+  before_action :buyer_user, only: [:show]
+  before_action :set_time, only: [:pay]
 
+  require "payjp"
 
+  # 購入画面表示
+  def show
+    if @card.present?                                                       # テーブルからpayjpの顧客IDを検索
+      customer = Payjp::Customer.retrieve(@card.customer_id)                # 保管した顧客IDでpayjpから情報取得
+      @default_card_information = customer.cards.retrieve(@card.card_id)    # 保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+    end
+  end
 
+  # 購入メソッド
+  def pay
+    if @card.blank?
+      flash[:no_card] = "カードを登録してください"
+      redirect_to new_user_credit_card_path(current_user.id)       # render "show" セカンドオプション
+    else
+      Payjp::Charge.create(
+        :amount => @item.price,                      # 支払金額を入力
+        :customer => @card.customer_id,              # 顧客ID
+        :currency => 'jpy',                          # 日本円
+      )
+      @item.update(buyer_id: current_user.id, deal_close_date: @time)
+      redirect_to root_path                          # 購入完了後、トップページに遷移
+    end
+  end
 
+  private
 
+  # 商品情報を取得
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
+  # 出品者を購入不可にする 
+  def correct_user
+    if current_user.id == @item.seller_id
+      redirect_to root_path
+    end
+  end
 
+  # SOLDOUT商品は購入不可にする
+  def buyer_user
+    if @item.buyer_id.present?
+      redirect_to root_path
+    end
+  end
 
+  # PAYJPとの通信キー取得
+  def secret_key
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+  end
 
+  # クレジットカード情報取得
+  def set_card
+    @card = CreditCard.where(user_id: current_user.id).first
+  end
+
+  # 現在時刻を取得(deal_close_date用)
+  def set_time
+    @time = Time.current
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,48 @@
 Rails.application.routes.draw do
   root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :new, :show, :create] do
+  
+  resources :items, only: [:index, :new, :show, :create, :update] do
+    resources :purchase, only: [:show] do
+      collection do
+        get "show", to: "purchase#show"
+        post "pay", to: "purchase#pay"
+      end
+    end
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
-    resources :purchase, only: [:show] do
-      collection do
-        get "show", to: "purchase#index"
-        post "pay", to: "purchase#pay"
-      end
-    end
   end
+
   devise_for :users
+  
   resources :profiles, only: [:index, :new, :create, :show, :edit, :update]
+  
   resources :users, only: [:show] do
     resources :credit_cards
   end
+
   resources :categories, only: [:index, :show]
-  resources :purchase, only: :show
+
 end
+
+
+
+
+
+
+
+
+  # resources :items, only: [:index, :new, :show, :create, :update] do
+  #   collection do
+  #     get 'get_category_children', defaults: { format: 'json' }
+  #     get 'get_category_grandchildren', defaults: { format: 'json' }
+  #   end
+  #   resources :purchase, only: [:show, :pay] do
+  #     collection do
+  #       get "show", to: "purchase#index"
+  #       post "pay", to: "purchase#pay"
+  #     end
+  #   end
+  # end
