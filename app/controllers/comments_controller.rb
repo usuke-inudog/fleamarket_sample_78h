6827215789d8dc05a9c1fa2bd996db7ed9482664
8hachi8 <bold.ryst@gmail.com>
2020-07-20 11:34:38 +0900

    Update comments_controller.rb
    prefixへ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,11 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new(comment_params)
     if comment.save
-      redirect_to "/items/#{comment.item.id}", notice: "コメントを投稿しました"
+      redirect_to item_path(comment.item.id),
+      notice: "コメントを投稿しました"
     else
-      redirect_to "/items/#{comment.item.id}", alert: "コメントを入力してください"
+      redirect_to item_path(comment.item.id),
+      alert: "コメントを入力してください"
     end
   end
 
