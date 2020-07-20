98d94eb8c95176c42032361921949142b4b35d9d
jo-taichi <strarf2215@yahoo.co.jp>
2020-07-20 11:03:19 +0900

    日時を日本(東京)時間に変更

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module FleamarketSample78h
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
   end
 end
