879ae5c50e36f3e6f826c249a021bb364953f449
oniyaku <nuda.veritas.1899@gmail.com>
2020-07-20 14:29:31 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :new, :show, :create, :edit, :update, :destroy] do
+  resources :items, only: [:new, :show, :create, :edit, :update, :destroy] do
     resources :purchase, only: [:show] do
       collection do
         post "pay", to: "purchase#pay"
